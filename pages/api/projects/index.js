a8174b6903747b99e221416f7a485861f3728a8c
Jared Cosulich <jared.cosulich@gmail.com>
2022-03-03 13:01:22 -0500

    filtering projects based on auth user id

--- pages/api/projects/index.js
+++ pages/api/projects/index.js
@@ -1,10 +1,43 @@
 
 
 import {
+  supabaseClient
+} from '../../../lib'
+
+import {
   prismaClient
 } from '../../../lib/server'
 
-export default async function handle(_req, res) {
-  const projects = await prismaClient.project.findMany({})
+export default async function handle(req, res) {
+  const { user, error  } = await supabaseClient.auth.api.getUserByCookie(req, res)
+
+  if (error?.status === 400) {
+    return res.status(400)
+  }
+
+  const projects = await prismaClient.project.findMany({
+    where: {
+      team: {
+        profiles: {
+          every: {
+            user_id: user.id
+          }
+        }
+      }  
+    },
+    select: {
+      id: true,
+      title: true,
+      team: {
+        select: {
+          title: true
+        }
+      }
+    }
+  })
+
+  // console.log('user', user)
+  // const { data, error } = await supabaseClient.from('project').select('*')
+  // res.json(data);
   res.json(projects);
 }
