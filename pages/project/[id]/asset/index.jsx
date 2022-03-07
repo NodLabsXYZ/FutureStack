198009eada51180a42d4b18dac8965c4285ae9bd
Jared Cosulich <jared.cosulich@gmail.com>
2022-03-07 09:25:49 -0500

    example supabase interaction for assets

--- pages/project/[id]/asset/index.jsx
+++ pages/project/[id]/asset/index.jsx
@@ -2,8 +2,20 @@ import { supabaseClient } from "../../../../lib";
 
 const AssetPage = () => {
   
-  const saveAsset = () => {
-    supabaseClient.from()
+  const saveAsset = async () => {
+    const { body, error } = await supabaseClient.from('asset').insert({
+      image_uri:            '',
+      metadata:             '',
+      onchain_image_uri:    '',
+      onchain_metadata_uri: '',
+      project_id:         ''
+    });
+
+    console.log("ASSET INSERTED", body, error);
+
+    const { data } = await supabaseClient.from('asset').select('*');
+
+    console.log("ASSETS", data);
   }
 
   return (
