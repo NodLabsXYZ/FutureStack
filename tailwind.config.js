100553278a736151402ea86b0ff8ee585fa97a9d
Jared Cosulich <jared.cosulich@gmail.com>
2022-03-16 14:33:34 -0400

    cleaning up project

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,11 @@
 module.exports = {
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
+  content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  darkMode: 'media',
   theme: {
     extend: {
       fontFamily: {
       }
     }
-  },
-  variants: {
-    extend: {},
   },
   plugins: [require('@tailwindcss/typography')]
 }
