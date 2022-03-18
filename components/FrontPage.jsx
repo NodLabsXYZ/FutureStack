80cdf54cfe47f8f2ef85837e97715e602298e170
Jared Cosulich <jared.cosulich@gmail.com>
2022-03-18 07:26:04 -0400

    working on front page

--- components/FrontPage.jsx
+++ components/FrontPage.jsx
@@ -6,7 +6,7 @@ const FrontPage = () => {
   return (
     <div className='p-12 text-2xl font-thin text-white'>
       
-      <div className='pt-12 w-96 mx-auto'>
+      <div className='pt-24 w-96 mx-auto'>
         <div>web3 infrastructure and services</div>
         <div className='font-semibold'>
           for developers and startups 
@@ -17,7 +17,7 @@ const FrontPage = () => {
         </div>
       </div>
 
-      <div className='mt-36 ml-48 w-96'>
+      <div className='mt-48 ml-48 w-96'>
         <div className='font-semibold'>
           For a startup or idea to make it 
           in web3, your timing matters. 
@@ -27,7 +27,7 @@ const FrontPage = () => {
           flexibility when developing and deploying 
           new web3 products. 
         </div>
-        <div className='rotate-[27deg] absolute right-72 top-60'>
+        <div className='rotate-[27deg] absolute right-72 top-72'>
           <Image
             src="/images/ethereum-logo.png"
             alt="Ethereum Logo"
@@ -38,7 +38,7 @@ const FrontPage = () => {
         </div>
       </div>
 
-      <div className='mt-36 ml-60 w-1/2'>
+      <div className='mt-48 ml-60 w-1/2'>
         <div className='float-left mr-6'>
           <Image
             src="/images/arweave-logo.png"
@@ -52,13 +52,13 @@ const FrontPage = () => {
             Arweave Uploader
           </div>
           <div className='mt-3'>
-            Store assets and metadata forever,<br/>
+            Store files on-chain forever,<br/>
             no recurring costs or hassle.
           </div>
           <div className='mt-3 text-lg'>
-            Increase velocity, maintain control and 
-            flexibility when developing and deploying 
-            web3 products. 
+          Leverage Arweave&apos;s permanent storage 
+          with our steamlined uploading. 
+          Pay once - store forever.
           </div>          
         </div>
         <div className='text-center mt-6'>
@@ -75,6 +75,74 @@ const FrontPage = () => {
             </NextLink>
           </TWButton>
         </div>        
+      </div>
+
+      <div className='mt-48 flex justify-between relative'>
+        <div className='opacity-30'>
+          <Image
+            src="/images/stacks.png"
+            alt="Stacks"
+            width={496}
+            height={377}          
+          />
+        </div>
+        <div></div>
+        <div className='float-right'>
+          <div className='font-semibold'>
+            Private Hosted Blockchain
+          </div>
+          <div className='mt-3'>
+            Share access with your team and customers,
+            <br/>
+            keep control and privacy.
+          </div>
+          <div className='mt-3 text-lg'>
+            Fully control a hosted blockchain. Manage access and
+            <br/>
+            easily automate testing. No more faucets!
+          </div>          
+          <div className='text-center mt-6'>
+            <TWButton
+              classMap={{
+                background: 'bg-indigo-800',
+                rounder: 'rounded-full'
+              }}
+            >
+              <NextLink href='/arweave'>
+                <a className='px-3 py-1 text-lg align-text-top font-light'>
+                  Join Waitlist
+                </a>
+              </NextLink>
+            </TWButton>
+          </div>        
+        </div>
+      </div>
+
+      <div className='mt-72 flex w-1/2 mx-auto'>
+        <div className=''>
+          <div className='font-semibold'>
+            New web3 services and infrastructure coming soon.
+          </div>
+          
+          <div className='mt-3 text-lg'>
+            Let us know what&apos;s slowing down your 
+            web3 development process.  
+          </div>          
+          <div className='text-center mt-6'>
+            <TWButton
+              classMap={{
+                background: 'bg-indigo-800',
+                rounder: 'rounded-full'
+              }}
+            >
+              <NextLink href='/arweave'>
+                <a className='px-3 py-1 text-lg align-text-top font-light'>
+                  Join Waitlist
+                </a>
+              </NextLink>
+            </TWButton>
+          </div>        
+        </div>
       </div>
     </div>
   )
--- components/FutureStackLayout.jsx
+++ components/FutureStackLayout.jsx
@@ -6,7 +6,7 @@ import {
 const FutureStackLayout = ({ user, gradient="", children }) => {
 
   return (
-    <div className='text-white' style={{ background: 'linear-gradient(#0C1322 10%, #5B5258 70%, #ECA33B 99%, #E55826 100%);'}}>
+    <div className='text-white' style={{ background: 'linear-gradient(#0C1322 10%, #5B5258 70%, #ECA33B 95%);'}}>
       <div className=''>
         <div className='container px-6 mx-auto'>
           <FutureStackHeader user={user} />
